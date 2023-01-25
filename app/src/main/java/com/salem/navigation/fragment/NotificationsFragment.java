a6ea1836542993c1b41f56b2e31452e4cab79b3a
salemawad <salemwd89@gmail.com>
2023-01-25 23:16:46 +0200

    Initial commit

---
+++ app/src/main/java/com/salem/navigation/fragment/NotificationsFragment.java
@@ -0,0 +1,28 @@
+package com.salem.navigation.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.salem.navigation.R;
+
+public class NotificationsFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        final TextView textView = root.findViewById(R.id.text_notifications);
+
+        return root;
+    }
+}
